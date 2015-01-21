7de558220851dc796b5d533734c8e5e74875b294
Szabó József <boby0201@freemail.hu>
2015-01-21 17:28:51 +0100

    Smooth scrolling, animation fix

--- js/header_animation.js
+++ js/header_animation.js
@@ -3,18 +3,24 @@
 **************************************************/
 
 function headerAnimation() {
-	var animationScrollY = 400;
+	var animationScrollY = 400;/* connect with the body padding-top value in index.css */
 	
-	var body = $("body");
+	var wrapper = $("#content-wrapper");
 	var blurred = $(".blurred");
 	var greyLayer = $(".grey-layer");
 	var distanceY = $(window).scrollTop();
-	
+
 	var opacity;
 	
+	// fix min-height for body - display scrollbar
+	var bodyMinHeight = ($("body").height() > 400*400/150) ? $("body").height() : 400*400/150;
+	$("body").css("min-height",bodyMinHeight+"px");
+	
 	if(distanceY < animationScrollY) {
-		// fake scroll effect
-		body.css("padding-top", distanceY+60+"px");
+		// disable scrolling
+		if(!wrapper.hasClass("fixed")){
+			wrapper.addClass("fixed");
+		}
 		
 		if(distanceY == 0){
 			greyLayer.css("opacity",1);
@@ -29,24 +35,30 @@ function headerAnimation() {
 			// fade blurred image
 			if(distanceY > (animationScrollY/2)) {
 				opacity = 1-(2 * ((distanceY-animationScrollY/2) / animationScrollY));
+				greyLayer.css("opacity",0);
 				blurred.css("opacity",opacity);
 			}
 		}
 	} else {
 		greyLayer.css("opacity",0);
 		blurred.css("opacity",0);
+		// enable scrolling
+		if(wrapper.hasClass("fixed")){
+			wrapper.removeClass("fixed");
+		}
 	}
 	
 	// floating text to top
 	$("#wrapper-header > .content > .inner-content").css("top",-1.25*distanceY);
 	// fade content
-	dynamixFade($("#logo"), distanceY);
-	dynamixFade($("#site-heading"), distanceY);
-	dynamixFade($("#site-text"), distanceY);
+	dynamicFade($("#logo"), distanceY);
+	dynamicFade($("#site-heading"), distanceY);
+	dynamicFade($("#site-text"), distanceY);
 }
 
-function dynamixFade(elem, scroll) {
-	var c = 60;
+// function for dynamic text fading in the header on scrolling
+function dynamicFade(elem, scroll) {
+	var c = 60;/* same as the top-header's height value */
 	var opacity = 1;
 	// top offset
 	var topOffset = elem.offset().top;
